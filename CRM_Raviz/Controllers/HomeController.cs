fb997e03fbf60601475fb127b03ca2b3caf39bec
vishaalupot <vishaalsupotofficial@gmail.com>
2024-04-25 14:30:13 +0400

    Added EditAllocation

--- CRM_Raviz/Controllers/HomeController.cs
+++ CRM_Raviz/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CRM_Raviz.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -31,5 +32,48 @@ namespace CRM_Raviz.Controllers
         {
             return View();
         }
+
+        public ActionResult _Records()
+        {
+
+            CPVDBEntities db = new CPVDBEntities();
+            List<RecordData> cases = db.RecordDatas.ToList();
+            return PartialView(cases);
+        }
+
+        public ActionResult RealEditAllocation(int id)
+        {
+            CPVDBEntities db = new CPVDBEntities();
+            RecordData recordData = db.RecordDatas.Find(id);
+            return View(recordData);
+        }
+
+        [HttpPost]
+        public ActionResult RealEditAllocation(FormCollection form)
+        {
+            CPVDBEntities db = new CPVDBEntities();
+
+            RecordData recordData = db.RecordDatas.Find(int.Parse(form["Id"].ToString()));
+
+            recordData.CustomerName = form["CustomerName"].ToString();
+            recordData.OS_Billing = form["OS_Billing"].ToString();
+            recordData.License_expiry = form["License_expiry"].ToString();
+            recordData.Nationality = form["Nationality"].ToString();
+            recordData.Email_1 = form["Email_1"].ToString();
+            recordData.Email_2 = form["Email_2"].ToString();
+            recordData.Email_3 = form["Email_3"].ToString();
+            recordData.Mobile1 = form["Mobile1"].ToString();
+            recordData.Mobile2 = form["Mobile2"].ToString();
+            recordData.Mobile3 = form["Mobile3"].ToString();
+            recordData.Disposition = form["Disposition"].ToString();
+            recordData.SubDisposition = form["SubDisposition"].ToString();
+            recordData.Comments = form["Comments"].ToString();
+            recordData.ChangeStatus = form["ChangeStatus"].ToString();
+            db.Entry(recordData).State = System.Data.Entity.EntityState.Modified;
+
+            db.SaveChanges();
+            return RedirectToAction("EditAllocation");
+        }
+
     }
 }
